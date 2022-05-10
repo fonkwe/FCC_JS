8a4afab7abb19a1d2b354a1ab623e5366c599315
fonkwe <williajeni@gmail.com>
2022-05-10 04:50:10 -0500

    some changes

--- Categorize_New_Member.js
+++ Categorize_New_Member.js
@@ -1,4 +1,19 @@
+/* Problem: 7 kyu
+Categorize New Member */
+/* Requirement: 
+ The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
 
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output
+Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.*/
+
+// Steps: Create a container to hold result.
+// 2. go through the data to get the x n y values.
+//3. use condition statements and return result.
 
 function openOrSenior(data){
     // Senior = >54 and >7
