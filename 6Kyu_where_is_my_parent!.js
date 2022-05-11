a677b8cb7f15c275b6146104b8cc8317cf0b9429
fonkwe <williajeni@gmail.com>
2022-05-10 23:30:22 -0500

    6kyu_codewar_challenge

---
+++ 6Kyu_where_is_my_parent!.js
@@ -0,0 +1,31 @@
+// Problem:6kyu Where is my parent!?(cry).
+/* Mothers arranged a dance party for the children in school. 
+At that party, there are only mothers and their children.
+All are having great fun on the dance floor when suddenly all the lights went out.
+It's a dark night and no one can see each other. 
+But you were flying nearby and you can see in the dark and have ability to teleport people anywhere you want.*/
+//Legend:-Uppercase letters stands for mothers, lowercase stand for their children, 
+//i.e. "A" mother's children are "aaaa".
+//-Function input: String contains only letters, uppercase letters are unique.
+/* Task:Place all people in alphabetical order where Mothers are followed by their children, i.e. "aAbaBb" => "AaaBbb".*/
+// Steps:
+// 1.converts the string into an array, with each character beeing one array element.
+// 2.sorts the array in alphabetic order.
+// 3.converted the array elements back to a string.
+
+function findChildren(dancingBrigade) {
+   let newArr = dancingBrigade.split('');
+   console.log(newArr);
+   oldArr = newArr.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()) || b.localeCompare(a));
+//The default sort order is ascending, 
+//built upon converting the elements into strings
+   return oldArr.join('');//returns a new string by concatenating all of the elements in an array
+}
+
+console.log(findChildren("beeeEBb")) //BbbEeee
+console.log(findChildren("beeeEBb")); //BbbEeee
+console.log(findChildren("uwwWUueEe")); //EeeUuuWww
+console.log(findChildren("abBA")); //AaBb
+console.log(findChildren("AaaaaZazzz")); //AaaaaaZzzz
+console.log(findChildren("CbcBcbaA")); //AaBbbCcc
+console.log(findChildren("xXfuUuuF")); //FfUuuuXx
--- thinking_longest_word.js
+++ thinking_longest_word.js
@@ -4,17 +4,22 @@ a list of words, and
 returns the length of the longest word in the list.*/
 // Requirement: Do not modify the input list.
 // Steps: 1. set a word in the list to be the longest.
-// 2. enter the list and compare each word length
+// 2. enter the list and compare each word length with the word set.
 //3. get the longest word.
 
 function longest(words) {
+    console.log("This is the type of words: " + typeof words);
     let longest = words[0];//set longest word to an initial value.
     for (let i = 0; i < words.length; i++) { // loop through the word list
         if (longest.length < words[i].length) { // compare the l.W.l to the individual List word length
             longest = words[i]; //if compare is passed then set a new value as l.word length.
+            console.log("This is the type of longest: " + typeof longest);
         }
     }
+    console.log("This is the type of longest.length: " + typeof longest.length)
     return longest.length;
 }
-
-console.log(longest(['simple', 'is', 'better', 'than', 'complex']));
+ 
+console.log(longest(['simple', 'is', 'better', 'than', 'complex']));// 7
+console.log(longest(['explicit', 'is', 'better', 'than', 'implicit']));//8
+console.log(longest(['beautiful', 'is', 'better', 'than', 'ugly']));//9
