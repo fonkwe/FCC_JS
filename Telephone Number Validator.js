8523ef023033aa9d958ffb310967e55e22d516cc
fonkwe <williajeni@gmail.com>
2022-06-06 06:26:16 -0500

    problem solving in js

--- Telephone Number Validator.js
+++ Telephone Number Validator.js
@@ -6,17 +6,35 @@
 //The following are examples of valid formats for US numbers 
 //(refer to the tests below for other variants):
 
+
 // Steps: 
+//1. remove unwanted characters that is return only numbers.
+//2. number format should be in the form 3numbers-3numbers-4numbers.
+//3. 1 can be added to the number format.
+//4. number length should be 11 characters long.
 
 
 
 // Implementation:
 
+// function telephoneCheck(str) {
+//   return str.replace(/[[2-9]^[(][)]\d{3}$\d{3}$-\d{4}$]/g, '');
+  // var re = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+
 function telephoneCheck(str) {
-    return true;
-  }
+    // const removedStr = /^([+]?1[\s]?)([(]{0,1}[0-9]{1,3}[)])(\d{3}$\d{3}$-\d{4}$])/;
+    // const re = /^\(?(\d{3})\)?[-. ]?(\d{3})[-. ]?(\d{4})$/;
+    const re = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+
+  return re.test(str);
+}
+
+telephoneCheck("555-555-5555");
   
-  telephoneCheck("555-555-5555");
+    
+
+  
+  console.log(telephoneCheck("555-555-5555"));
 
 
 
@@ -34,8 +52,8 @@ function telephoneCheck(str) {
 // telephoneCheck("555-5555") should return false.
 // telephoneCheck("5555555") should return false.
 // telephoneCheck("1 555)555-5555") should return false.
-// telephoneCheck("1 555 555 5555") should return true.
-// telephoneCheck("1 456 789 4444") should return true.
+console.log(telephoneCheck("(555)555-5555")); //should return true.
+console.log(telephoneCheck("1(555)555-5555"));//should return true
 // telephoneCheck("123**&!!asdf#") should return false.
 // telephoneCheck("55555555") should return false.
 // telephoneCheck("(6054756961)") should return false.
