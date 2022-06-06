1b409d2b21f3b040ff7457df67ca2f2851dcd26e
fonkwe <williajeni@gmail.com>
2022-06-06 04:51:15 -0500

    Problem solving in Js

---
+++ Telephone Number Validator.js
@@ -0,0 +1,55 @@
+// Problem: Telephone Number Validator
+// Description: Return true if the passed string looks like 
+//a valid US phone number.
+//The user may fill out the form field any way they choose as 
+//long as it has the format of a valid US number. 
+//The following are examples of valid formats for US numbers 
+//(refer to the tests below for other variants):
+
+// Steps: 
+
+
+
+// Implementation:
+
+function telephoneCheck(str) {
+    return true;
+  }
+  
+  telephoneCheck("555-555-5555");
+
+
+
+
+
+
+// Test:
+// telephoneCheck("555-555-5555") should return a boolean.
+// telephoneCheck("1 555-555-5555") should return true.
+// telephoneCheck("1 (555) 555-5555") should return true.
+// telephoneCheck("5555555555") should return true.
+// telephoneCheck("555-555-5555") should return true.
+// telephoneCheck("(555)555-5555") should return true.
+// telephoneCheck("1(555)555-5555") should return true.
+// telephoneCheck("555-5555") should return false.
+// telephoneCheck("5555555") should return false.
+// telephoneCheck("1 555)555-5555") should return false.
+// telephoneCheck("1 555 555 5555") should return true.
+// telephoneCheck("1 456 789 4444") should return true.
+// telephoneCheck("123**&!!asdf#") should return false.
+// telephoneCheck("55555555") should return false.
+// telephoneCheck("(6054756961)") should return false.
+// telephoneCheck("2 (757) 622-7382") should return false.
+// telephoneCheck("0 (757) 622-7382") should return false.
+// telephoneCheck("-1 (757) 622-7382") should return false.
+// telephoneCheck("2 757 622-7382") should return false.
+// telephoneCheck("10 (757) 622-7382") should return false.
+// telephoneCheck("27576227382") should return false.
+// telephoneCheck("(275)76227382") should return false.
+console.log(telephoneCheck("2(757)6227382"));// should return false.
+console.log(telephoneCheck("2(757)622-7382"));// should return false.
+console.log(telephoneCheck("555)-555-5555"));// should return false.
+console.log(telephoneCheck("(555-555-5555"));// should return false.
+console.log(telephoneCheck("(555)5(55?)-5555")); //should return false.
+console.log(telephoneCheck("55 55-55-555-5"));// should return false.
+console.log(telephoneCheck("11 555-555-5555"));// should return false.
